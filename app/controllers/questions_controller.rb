44720664210abb5c0b534f5470b456fe9268ceb4
Laia Grobe De Luca <laia.gdla@gmail.com>
2023-04-15 13:03:51 +0200

    e

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,13 @@
 class QuestionsController < ApplicationController
   def ask
-    @right_question = 'I am going to work'
   end
 
   def answer
     @answers = ['Great!', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to work!"]
     @question = params[:question]
+    @right_question = 'I am going to work'
 
-    @reply = if @question.equal? @right_question
+    @reply = if @question.eql? @right_question
                @answers[0]
              elsif @question.include? '?'
                @answers[1]
